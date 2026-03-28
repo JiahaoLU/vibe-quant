06249e28761fd6e93b42d6e435c4f46f3b36c2a1
JiahaoLU <lujiahao8146@gmail.com>
2026-03-29 00:17:56 +0100

    fix: export StrategyBase from trading/base

--- trading/base/__init__.py
+++ trading/base/__init__.py
@@ -1,6 +1,6 @@
 from .data      import DataHandler
 from .execution import ExecutionHandler
 from .portfolio import Portfolio
-from .strategy  import Strategy
+from .strategy  import Strategy, StrategyBase
 
-__all__ = ["DataHandler", "ExecutionHandler", "Portfolio", "Strategy"]
+__all__ = ["DataHandler", "ExecutionHandler", "Portfolio", "Strategy", "StrategyBase"]
